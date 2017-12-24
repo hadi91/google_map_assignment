fc2d1178683c1d799a12f047a796e4cecc1cc0c0
hadi91 <ahmad_nurhadi@hotmail.com>
2017-12-24 22:48:02 +0800

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'maps#index'
 
   resources :maps
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_scope :user do
+    authenticated :user do
+      root 'maps#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
