44283299e118f61dfab3687df470279fbb8c933b
hadi91 <ahmad_nurhadi@hotmail.com>
2017-12-26 16:51:37 +0800

    edit autocomplete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -69,22 +69,21 @@ function geocodeAddress(geocoder, resultsMap) {
     }
   });
 }
+
 function geolocate() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var geolocation = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-      var circle = new google.maps.Circle({
-        center: geolocation,
-        radius: 1000
-      });
-      var addressAuto = document.getElementById('address');
-      var autocomplete = new google.maps.places.Autocomplete(addressAuto);
-      autocomplete.setBounds(circle.getBounds());
+  navigator.geolocation.getCurrentPosition(function(position) {
+    var geolocation = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    };
+    var circle = new google.maps.Circle({
+      center: geolocation,
+      radius: 1000
     });
-  }
+    var addressAuto = document.getElementById('address');
+    var autocomplete = new google.maps.places.Autocomplete(addressAuto);
+    autocomplete.setBounds(circle.getBounds());
+  });
 }
 
 function initialize() {
